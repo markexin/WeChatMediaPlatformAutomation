32275b91db8240336a6bef4707855f1ef5b94721
maqun <maqun1@xiaomi.com>
2025-02-12 15:02:33 +0800

    update: 更新新版的微信公众号,增加AI图片选取

---
+++ README.md
@@ -0,0 +1,139 @@
+# WeChatMediaPlatformAutomation
+
+一款在微信公众号( https://mp.weixin.qq.com )自动预览/发布文章的命令行工具。
+
+## 如何使用
+
+1. 安装：`npm install wechat-mp-automation -g `
+
+2. 打开命令行执行：
+   1. 配置文件方式：
+      1. `wechat-mp-automation -C YOUR_CONFIG_JSON_FILE_PATH`
+   2. 非配置文件方式：
+      1. 非原创：`wechat-mp-automation -t [标题] -a [作者] -u [账号] -p [密码]`
+      2. 声明原创：`wechat-mp-automation -t [标题] -a [作者] -u [账号] -p [密码] -o`
+      3. 其余参数，参看如下帮助文档👇
+   
+3. 过程中的扫码：
+
+   1. 一次扫码，验证身份后登录
+   2. 若设置了只预览不发布（1.2.0 起支持 `--preview`），无需扫码即可预览文章
+   3. 1.2.0 前版本或未设置预览的情况，还需一次扫码，确认群发（如群发前，未异常报错的话）
+
+> 本工具不以任何形式保存账号和密码！！！
+
+> puppeteer 安装失败可以参考[这里](https://github.com/cnpm/cnpmjs.org/issues/1246#issuecomment-454268958)
+
+## 帮助文档
+
+```git
+$ wechat-mp-automation -h
+Usage: wechat-mp-automation [options]
+
+Options:
+  -V, --version                 output the version number
+  -C, --configPath [xxx]        配置文件的本地路径（支持所有自定义参数）
+  -t, --title [xxx]             文章标题
+  -a, --author [xxx]            文章作者
+  -c, --content [xxx]           文章内容[可选]，默认从粘贴板复制
+  -u, --username [xxx]          公众号账号
+  -p, --password [xxx]          公众号密码
+  -o, --original                声明原创[可选]
+  --preview                     预览而不发布[可选]
+  --preview_username [xxx~yyy]  预览名单[可选]，以~间隔多个微信号（自行保证微信号已关注公众号）
+  --skip_typing                 跳过文章标题、作者、文章的填写和封面图片选择（声明原创除外）[可选]
+  --last_edit                   选中最近编辑的文章[可选]，请自行确保当前有“最近编辑”的文章
+  -h, --help                    output usage information
+```
+
+config.json demo:
+```json
+{
+  "title": "test",
+  "author": "小铁匠Linus",
+  "username": "YOUR_USERNAME",
+  "password": "YOUR_PASSWORD",
+  "original": "true"
+}
+```
+
+## CHANGELOG
+
+<details>
+<summary>1.3.2</summary>
+</br>
+<p>1. 适配新版本的群发界面</p>
+<p>2. 优化二维码的截取展示</p>
+<p>3. 优化参数读取</p>
+</details>
+
+<details>
+<summary>1.3.1</summary>
+</br>
+<p>1. 修复点击封面图片选择失效的问题</p>
+</details>
+
+<details>
+<summary>1.3.0</summary>
+</br>
+<p>1. 支持新版本的公众号后台</p>
+</details>
+
+<details>
+<summary>1.2.0</summary>
+</br>
+<p>1. 支持预览文章，而不发布</p>
+<p>2. 选择预览时，支持指定预览的微信号名单（自行保证微信号已关注公众号）</p>
+<p>3. 支持跳过填写内容，建议用于二次预览或发布的情况</p>
+<p>4. 支持选择最近编辑的文章功能，避免每次都新建群发</p>
+<p>5. 未指定文章内容时，采用剪贴板粘贴的方式填入内容，替换原模拟键盘输入的方式</p>
+</details>
+
+<details>
+<summary>1.1.1</summary>
+</br>
+<p>1. 登录默认选择账号密码登录</p>
+<p>2. 官网页面元素的更正，恢复群发流程</p>
+</details>
+
+<details>
+<summary>1.1.0</summary>
+</br>
+<p>1. 支持使用 JSON 格式的本地配置文件作为参数，避免命令行泄漏关键信息</p>
+<p>2. 支持在发布过程中展示文章内容</p>
+</details>
+
+## Demo
+
+1. 利用**文章内容默认从粘贴板复制**的特性，配合一行命令生成公众号内容的工具 [wechat-format-cli](https://github.com/LinusLing/wechat-format-cli) 使用更香
+
+![cli.png](https://i.loli.net/2020/06/19/GDEwdxrHnTVRyZe.png)
+
+2. 预览最近编辑的文章（用于上一次异常报错或想查看最近一次编辑的文章）
+
+![1.2.0.png](https://i.loli.net/2020/06/19/FzryZdN5VsXoplw.png)
+
+2. 自动发布成功的流程示例
+
+![CorrectResult.png](https://i.loli.net/2019/07/23/5d371a7398b4141770.png)
+
+2. 发布失败流程及失败原因
+
+![error_progress.png](https://i.loli.net/2019/07/23/5d371a73c0f5f58172.png)
+
+![ErrorResult.png](https://i.loli.net/2019/07/23/5d37086e81ff423521.png)
+
+## TODO
+
+1. 通过指定特定文件来上传文章内容
+2. 文章发布前的设置可进行自定义（比如~~预览~~、图片选择等）
+3. 支持更多种类的创作（~~图文消息~~、文字消息、视频消息、音频消息、图片消息、转载等）
+
+## Issues
+
+[意见与建议](https://github.com/LinusLing/WeChatMediaPlatformAutomation/issues/new)
+
+## 赞赏
+
+<div style="float:left;border:solid 1px 000;margin:2px;"><img src="https://i.loli.net/2019/07/23/5d370dca1cf1911283.jpg" width="300" height="450" ></div>
+<div style="float:left;border:solid 1px 000;margin:2px;"><img src="https://i.loli.net/2019/07/23/5d370dcd23ed242202.jpg" width="300" height="450" ></div>
---
+++ automation.js
@@ -0,0 +1,420 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+const open = require('open')
+const clipboardy = require('node-clipboardy');
+const LocalDate = require('js-localdate-plus');
+var fs = require('fs');
+const program = require('commander');
+
+program
+    .version('1.3.2')
+    .usage(' [options]')
+    .option('-C, --configPath [xxx]', '配置文件的本地路径（支持所有自定义参数）')
+    .option('-t, --title [xxx]', '文章标题')
+    .option('-a, --author [xxx]', '文章作者')
+    .option('-pr, --prompt [xxx]', 'AI 文章配图prompt')
+    .option('-c, --content [xxx]', '文章内容[可选]，默认从粘贴板复制')
+    .option('-u, --username [xxx]', '公众号账号')
+    .option('-p, --password [xxx]', '公众号密码')
+    .option('-o, --original', '声明原创[可选]')
+    .option('--preview', '预览而不发布[可选]')
+    .option('--preview_username [xxx~yyy]', '预览名单[可选]，以~间隔多个微信号（自行保证微信号已关注公众号）')
+    .option('--skip_typing', '跳过文章标题、作者、文章的填写和封面图片选择（声明原创除外）[可选]')
+    .option('--last_edit', '选中最近编辑的文章[可选]，请自行确保当前有“最近编辑”的文章')
+    .parse(process.argv);
+
+let title;
+let author;
+let content;
+let username;
+let password;
+let original;
+let preview;
+let preview_username;
+let skip_typing;
+let last_edit;
+let prompt;
+
+if (program.configPath !== undefined) {
+    try {
+        const contents = fs.readFileSync(program.configPath);
+        const jsonContent = JSON.parse(contents);
+        title = jsonContent.title || undefined;
+        author = jsonContent.author || undefined;
+        content = jsonContent.content || undefined;
+        username = jsonContent.username || undefined;
+        password = jsonContent.password || undefined;
+        original = jsonContent.original || undefined;
+        preview = jsonContent.preview || undefined;
+        prompt = jsonContent.prompt || undefined;
+        preview_username = program.preview_username && program.preview_username.split("~") || undefined;
+        skip_typing = jsonContent.skip_typing || undefined;
+        last_edit = jsonContent.last_edit || undefined;
+        console.log('读取配置文件成功');
+    } catch (error) {
+        console.log('读取配置文件失败');
+        console.log(error);
+    }
+}
+
+console.log("----------配置内容 begin----------");
+
+if (title === undefined) {
+    if (program.title === undefined) {
+        console.log('缺少文章标题， -h 了解如何使用');
+        return;
+    } else {
+        title = String(program.title);
+        console.log('文章标题：' + title);
+    }
+}
+
+if (author === undefined) {
+    if (program.author === undefined) {
+        console.log('缺少文章作者， -h 了解如何使用');
+        return;
+    } else {
+        author = String(program.author);
+        console.log('文章作者：' + author);
+    }
+}
+if (content === undefined) {
+    if (program.content === undefined) {} else {
+        content = String(program.content);
+    }
+}
+if (username === undefined) {
+    if (program.username === undefined) {
+        console.log('缺少公众号账号， -h 了解如何使用');
+        return;
+    } else {
+        username = String(program.username);
+    }
+}
+if (password === undefined) {
+    if (program.password === undefined) {
+        console.log('缺少公众号密码， -h 了解如何使用');
+        return;
+    } else {
+        password = String(program.password);
+    }
+}
+if (original === undefined) {
+    if (program.original === undefined) {} else {
+        original = program.original;
+    }
+}
+
+if (prompt === undefined) {
+    if (program.prompt === undefined) {
+        console.log('缺少公众号配图prompt， -h 了解如何使用');
+        return;
+    } else {
+        prompt = String(program.prompt);
+    }
+}
+console.log((!original ? "文章不" : "文章将") + "声明原创");
+
+if (preview === undefined) {
+    if (program.preview === undefined) {} else {
+        preview = program.preview;
+        console.log("文章不会发布，只预览");
+    }
+}
+
+if (preview_username === undefined) {
+    if (program.preview_username === undefined) {} else {
+        preview_username = program.preview_username.split("~");
+        console.log("可预览本文章的微信号：" + preview_username + "（关注公众号后，才能接收图文消息预览）");
+    }
+}
+
+if (skip_typing === undefined) {
+    if (program.skip_typing === undefined) {} else {
+        skip_typing = program.skip_typing;
+        console.log("将跳过文章标题、作者、文章的填写和封面图片选择（声明原创除外）");
+    }
+}
+
+if (last_edit === undefined) {
+    if (program.last_edit === undefined) {} else {
+        last_edit = program.last_edit;
+    }
+}
+console.log(last_edit ? "将选中最近编辑的文章" : "将新建群发的文章");
+
+console.log("----------配置内容 end----------");
+
+const url = "https://mp.weixin.qq.com/"
+
+function autoLogin() {
+    return new Promise(async (resolve, reject) => {
+        const browserConfig = process.env.SHOW_BROWSER ? {
+            headless: false,
+            slowMo: 100
+        } : {}
+        browserConfig['defaultViewport'] = null; // 页面最大化
+        const browser = await puppeteer.launch(browserConfig);
+        let page = await browser.newPage();
+        await page.setViewport({
+            width: 1200,
+            height: 890,
+        });
+
+        try {
+            let clickAndWaitForTarget = async (clickSelector, page, browser) => {
+                const pageTarget = page.target(); //save this to know that this was the opener
+                await page.click(clickSelector); //click on a link
+                const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget); //check that you opened this page, rather than just checking the url
+                const newPage = await newTarget.page(); //get the page object
+                // await newPage.once("load",()=>{}); //this doesn't work; wait till page is loaded
+                await newPage.waitForSelector("body"); //wait for page to be loaded
+
+                return newPage;
+            };
+
+            // 打开首页
+            console.log("正在打开登录首页...");
+            await page.goto(url);
+
+            // 登录
+            console.log("正在登录...");
+            const element = await page.$('[class="login__type__container login__type__container__scan"]');
+            if (element) {
+                await page.click('#header > div.banner > div > div > div.login__type__container.login__type__container__scan > a')
+            }
+
+            await page.waitForSelector('#header > div.banner > div > div > div.login__type__container.login__type__container__account > form > div.login_btn_panel > a');
+            //type the name
+            await page.focus('#header > div.banner > div > div > div.login__type__container.login__type__container__account > form > div.login_input_panel > div:nth-child(1) > div > span > input')
+            await page.keyboard.type(username);
+            //type the pwd
+            await page.focus('#header > div.banner > div > div > div.login__type__container.login__type__container__account > form > div.login_input_panel > div:nth-child(2) > div > span > input')
+            await page.keyboard.type(password);
+            await page.waitFor(50);
+            //Click on the submit button
+            await page.click('#header > div.banner > div > div > div.login__type__container.login__type__container__account > form > div.login_btn_panel > a')
+
+            // 扫码登录
+            console.log("扫码登录中...");
+            const IMAGE_SELECTOR = '#app > div.weui-desktop-layout__main__bd > div > div.js_scan.weui-desktop-qrcheck > div.weui-desktop-qrcheck__qrcode-area > div > img'
+            await page.waitForSelector(IMAGE_SELECTOR);
+            await page.waitFor(500);
+            await page.screenshot({
+                path: 'screenshot.png',
+                clip: {
+                    x: 390,
+                    y: 270,
+                    width: 420,
+                    height: 350
+                }
+            });
+            open('screenshot.png');
+
+            if (last_edit) {
+                // 最近编辑
+                console.log("打开最近编辑的文章中...");
+                const LAST_EDIT_BUTTON_SELECTOR = '#app > div.main_bd > div:nth-child(5) > div.weui-desktop-panel__bd > div > div > div:nth-child(1) > span:nth-child(1) > div > div > div.weui-desktop-card__action > div > div.weui-desktop-tooltip__wrp.weui-desktop-link';
+                await Promise.race([
+                    page.waitForSelector(LAST_EDIT_BUTTON_SELECTOR)
+                ]);
+
+                await page.hover(LAST_EDIT_BUTTON_SELECTOR);
+                await page.waitFor(500);
+                page = await clickAndWaitForTarget(LAST_EDIT_BUTTON_SELECTOR, page, browser);
+
+                // 删除扫码登录截图
+                fs.unlinkSync('screenshot.png');
+
+                await page.waitFor(5000);
+            } else {
+                // 新建群发（图文消息 div:nth-child(1)）
+                console.log("新建群发文章中...");
+                const NEW_POST = '#app > div.main_bd_new > div:nth-child(3) > div.weui-desktop-panel__bd > div > div:nth-child(2)'
+                const element2 = await Promise.race([
+                    page.waitForSelector(NEW_POST)
+                ]);
+
+                await page.waitFor(500);
+                page = await clickAndWaitForTarget(NEW_POST, page, browser);
+
+                // 删除扫码登录截图
+                fs.unlinkSync('screenshot.png');
+
+                await page.waitFor(5000);
+            }
+
+            if (!skip_typing) {
+                // 文章标题
+                console.log("正在填写文章标题...");
+                await page.click('#title');
+                await page.waitFor(100);
+                await page.keyboard.type(String(title));
+                await page.waitFor(100);
+
+                // 文章作者
+                console.log("正在填写文章作者...");
+                await page.keyboard.press('Tab', {
+                    delay: 100
+                });
+                await page.keyboard.type(String(author));
+                await page.waitFor(100);
+
+                // 文章内容
+                console.log("正在填写文章内容...");
+                await page.keyboard.press('Tab', {
+                    delay: 100
+                });
+
+                var pasted_content;
+                if (content) {
+                    // 指定文章内容时，模拟键盘输入内容
+                    pasted_content = content;
+                    await page.keyboard.type(String(pasted_content));
+                } else {
+                    // 未指定文章内容时，采用剪贴板粘贴的方式填入内容
+                    pasted_content = await clipboardy.read();
+                    // https://stackoverflow.com/questions/11750447/performing-a-copy-and-paste-with-selenium-2#answer-41046276
+                    // https://github.com/puppeteer/puppeteer/blob/56742ebe8cbb353d7739faee358f60832ef113e5/src/USKeyboardLayout.ts
+                    await page.keyboard.down('ShiftLeft')
+                    await page.keyboard.press('Insert')
+                    await page.keyboard.up('ShiftLeft')
+                }
+                await page.waitFor(100);
+
+                console.log("----------文章内容 begin----------");
+                console.log(pasted_content)
+                console.log("----------文章内容 end----------");
+
+                // 封面图片选择
+                console.log("正在自动选择封面图片...");
+                await page.hover('#js_cover_area > div.select-cover__btn.js_cover_btn_area');
+                await page.waitFor(500);
+                await page.click('.js_aiImage');
+                await page.waitFor(500);
+                // 使输入框获得焦点
+                await page.focus('.chat_textarea');
+                await page.waitFor(500);
+                await page.keyboard.type(String(prompt));
+                await page.click('.weui-desktop-btn_primary');
+                await page.waitFor(10000);
+                // 选择默认图片
+                await page.click('.chat-ai-list > .chat-ai-item:last-child .ai-image-item .ai-image-finetuning-btn:last-child');
+                await page.waitFor(500);
+                await page.click('.weui-desktop-btn_primary');
+                // 下一步
+                await page.waitFor(1200);
+            }
+
+            if (original) {
+                // 声明原创
+                console.log("正在声明原创...");
+
+                await page.evaluate(() => {
+                    document.querySelector('#js_original > div.unorigin.js_original_type > div.setting-group__content > a').click();
+                });
+                await page.waitForSelector("label[for='js_copyright_agree'");
+                await page.click('body > div.dialog_wrp.simple.align_edge.original_dialog.ui-draggable > div > div.dialog_bd > div.step_panel.step_agreement.js_step_panel > div > div > div > div.tool_area.new-tool_area > label > i');
+                await page.click('body > div.dialog_wrp.simple.align_edge.original_dialog.ui-draggable > div > div.dialog_ft > span:nth-child(1) > button');
+                await page.waitFor(50);
+                await page.click('#js_original_article_type > div > a');
+                await page.waitFor(50);
+                await page.click('#js_original_article_type > div > div > div > div.weui-desktop-dropdown__list__cascade__container.js_scroll_area.js_data > dl > dd > dl:nth-child(2) > dt');
+                await page.waitFor(50);
+                await page.click('body > div.dialog_wrp.simple.align_edge.original_dialog.ui-draggable > div > div.dialog_ft > span:nth-child(3) > button');
+                await page.waitFor(500);
+            } else {
+                await page.waitFor(500);
+            }
+
+            if (preview) {
+                // 预览
+                console.log("正在预览文章...");
+                const PREVIEW_BTN = "#js_preview > button";
+                await page.waitForSelector(PREVIEW_BTN);
+                await page.click(PREVIEW_BTN);
+                await page.waitFor(500);
+
+                const element = await page.$('[class="weui-desktop-form-tag__name"]');
+                if (!element) {
+                    console.log("正在填写预览名单...");
+                    // 没有默认预览的名单，则添加 preview_username 中的名单
+                    await page.waitForSelector('#js_preview_wxname');
+                    await page.focus('#js_preview_wxname');
+                    for (const key in preview_username) {
+                        const username = preview_username[key];
+                        await page.keyboard.type(username);
+                        await page.keyboard.press('Enter', {
+                            delay: 100
+                        });
+                        await page.waitFor(1500);
+                    }
+                }
+
+                // 预览确认
+                console.log("预览确认中...");
+                const PREVIEW_CONFIRM_BTN = "body > div.dialog_wrp.label_block.wechat_send_dialog.ui-draggable > div > div.dialog_ft > span.btn.btn_primary.btn_input.js_btn_p > button"
+                await page.waitForSelector(PREVIEW_CONFIRM_BTN);
+                await page.click(PREVIEW_CONFIRM_BTN);
+
+                await page.waitFor(500);
+                console.log("预览发布成功。");
+            } else {
+                // 保存并转发
+                console.log("正在保存文章并转发...");
+                const SEND_BTN = "#js_send > button";
+                await page.waitForSelector(SEND_BTN);
+                await page.click(SEND_BTN);
+                await page.waitFor(500);
+
+                // 群发+确认群发
+                console.log("扫码确认群发中...");
+                const SCAN_SEND_BTN = "div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__ft > div > div.weui-desktop-popover__wrp > button";
+                await page.waitForSelector(SCAN_SEND_BTN);
+                await page.click(SCAN_SEND_BTN);
+                const CONFIRM_SEND_BTN = "div.weui-desktop-dialog__wrp > div > div.weui-desktop-dialog__ft > div > button.weui-desktop-btn.weui-desktop-btn_primary";
+                await page.waitForSelector(CONFIRM_SEND_BTN);
+                await page.waitFor(2000);
+                await page.click(CONFIRM_SEND_BTN);
+
+                // 等待确认二维码
+                await page.waitForSelector('body > div.dialog_wrp.ui-draggable > div > div.dialog_bd > div > div > div.qrcode_wrp > img')
+                await page.waitFor(500);
+                await page.screenshot({
+                    path: 'confirmSend.png',
+                    clip: {
+                        x: 260,
+                        y: 640,
+                        width: 280,
+                        height: 330
+                    }
+                });
+                open('confirmSend.png');
+
+                // 等待发布成功页面展示
+                const MAIN_BD = "#app > div.main_bd";
+                await page.waitForSelector(MAIN_BD);
+                await page.waitFor(500);
+                console.log("群发发布成功。");
+
+                // 删除扫码确认发布截图
+                fs.unlinkSync('confirmSend.png');
+            }
+
+            // 结束
+            browser.close();
+            return resolve();
+        } catch (e) {
+            // 异常时截图保存
+            await page.screenshot({
+                path: 'ErrorResult.png',
+            });
+            console.log("发生异常，详情请见 ErrorResult.png");
+            // 结束
+            browser.close();
+            return reject(e);
+        }
+    })
+}
+autoLogin().catch(console.error);
